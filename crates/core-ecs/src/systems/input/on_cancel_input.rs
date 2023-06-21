1ee6e4f519e1a3c89b50641e8e988fdb328515e2
schr3da <strieder.georg@gmail.com>
2023-06-21 02:00:26 +0200

    🚑 fixed cancel event

--- crates/core-ecs/src/systems/input/on_cancel_input.rs
+++ crates/core-ecs/src/systems/input/on_cancel_input.rs
@@ -6,7 +6,7 @@ use crate::prelude::*;
 
 pub fn on_cancel_input_system(subscriber: Res<Subscriber>) {
     match subscriber.next_event {
-        Some(SendEvents::Input(InputEvents::New)) => {}
+        Some(SendEvents::Input(InputEvents::Cancel)) => {}
         _ => return,
     };
 
